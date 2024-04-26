fa28290a6376628a7c7b60b3016bed77459d8172
shailjasoni2005 <144000431+shailjasoni2005@users.noreply.github.com>
2024-04-26 03:50:45 +0000

    cpp programs

---
+++ .vscode/DSA/program10.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun(int n)
+{
+    if(n>0)
+    {
+        cout<<n<<" ";
+        fun(n-1);
+    }
+}
+
+int main(){
+    int x;
+    cout<<"Enter any number";
+    cin>>x;
+    fun(x);
+
+    return 0;
+}
